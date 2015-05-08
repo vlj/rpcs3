0011aec42d6cf3b6d363a4851e72b57595642484
Peter Tissen <dravorek@gmail.com>
2015-05-08 21:03:05 +0200

    delete static linking tests

--- rpcs3/Emu/RSX/GL/GLFragmentProgram.cpp
+++ rpcs3/Emu/RSX/GL/GLFragmentProgram.cpp
@@ -124,7 +124,7 @@ std::string GLFragmentDecompilerThread::Format(const std::string& code)
 	const std::pair<std::string, std::function<std::string()>> repl_list[] =
 	{
 		{ "$$", []() -> std::string { return "$"; } },
-		{ "$0", /*[this]() -> std::string {return GetSRC<SRC0>(src0);} },*/std::bind(std::mem_fn(&GLFragmentDecompilerThread::GetSRC<SRC0>), *this, src0) },
+		{ "$0", [this]() -> std::string {return GetSRC<SRC0>(src0);} },//std::bind(std::mem_fn(&GLFragmentDecompilerThread::GetSRC<SRC0>), *this, src0) },
 		{ "$1", [this]() -> std::string {return GetSRC<SRC1>(src1);} },//std::bind(std::mem_fn(&GLFragmentDecompilerThread::GetSRC<SRC1>), this, src1) },
 		{ "$2", [this]() -> std::string {return GetSRC<SRC2>(src2);} },//std::bind(std::mem_fn(&GLFragmentDecompilerThread::GetSRC<SRC2>), this, src2) },
 		{ "$t", std::bind(std::mem_fn(&GLFragmentDecompilerThread::AddTex), this) },
