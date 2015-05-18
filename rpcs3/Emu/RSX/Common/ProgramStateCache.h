f2de0d3cab6440b5f88fd68991180109415a4995
vlj <vljn.ovi@gmail.com>
2015-05-18 18:08:08 +0200

    Revert "RSX: Fix for ProgramStateCache"
    This reverts commit 305ea5d836be0eaf74678415675cc8517064675c.

--- rpcs3/Emu/RSX/Common/ProgramStateCache.h
+++ rpcs3/Emu/RSX/Common/ProgramStateCache.h
@@ -338,10 +338,12 @@ public:
 
 	const std::vector<size_t> &getFragmentConstantOffsetsCache(const RSXFragmentProgram *fragmentShader) const
 	{
-		binary2FS::const_iterator It = m_cacheFS.find(vm::get_ptr<void>(fragmentShader->addr));
+		binary2FS::const_iterator It = m_cacheFS.find(vm::get_ptr<void>(rsx_fp->addr));
 		if (It != m_cacheFS.end())
-			return It->second.FragmentConstantOffsetCache;
+		{
+			found = true;
+			return  It->second.FragmentConstantOffsetCache;
+		}
 		LOG_ERROR(RSX, "Can't retrieve constant offset cache");
-		return std::vector<size_t>();
 	}
 };
