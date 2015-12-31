1e4b2f30899163a7d02246ece43d705bb458d059
Vincent Lejeune <vljn.ovi@gmail.com>
2015-12-31 18:27:21 +0100

    fix for bleach ?

--- rpcs3/Emu/SysCalls/Modules/cellFont.cpp
+++ rpcs3/Emu/SysCalls/Modules/cellFont.cpp
@@ -478,7 +478,7 @@ s32 cellFontSetScalePoint()
 s32 cellFontSetupRenderEffectSlant()
 {
 	UNIMPLEMENTED_FUNC(cellFont);
-	return CELL_OK;
+	return -1;
 }
 
 s32 cellFontGraphicsSetLineRGBA()
@@ -508,7 +508,7 @@ s32 cellFontGraphicsSetupDrawContext()
 s32 cellFontSetupRenderEffectWeight()
 {
 	UNIMPLEMENTED_FUNC(cellFont);
-	return CELL_OK;
+	return -1;
 }
 
 s32 cellFontGlyphGetOutlineControlDistance()
@@ -552,7 +552,7 @@ s32 cellFontExtend(u32 a1, u32 a2, u32 a3)
 		//Something happens
 	}
 	//Something happens?
-	return CELL_OK;
+	return -1;
 }
 
 s32 cellFontRenderCharGlyphImageVertical()
@@ -585,12 +585,12 @@ s32 cellFontGraphicsGetDrawType()
 
 s32 cellFontGetKerning()
 {
-	throw EXCEPTION("");
+	return -1;//throw EXCEPTION("");
 }
 
 s32 cellFontGetRenderScaledKerning()
 {
-	throw EXCEPTION("");
+	return -1;//throw EXCEPTION("");
 }
 
 s32 cellFontGetRenderScalePixel()
@@ -620,7 +620,7 @@ s32 cellFontGetEffectWeight()
 
 s32 cellFontGetScalePixel()
 {
-	throw EXCEPTION("");
+	return -1;//throw EXCEPTION("");
 }
 
 s32 cellFontClearFileCache()
